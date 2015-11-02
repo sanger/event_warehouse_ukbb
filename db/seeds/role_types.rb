02c24212865328c7d4a3a2780418de0174c30107
James Glover <james.glover@sanger.ac.uk>
2015-11-02 11:55:34 +0000

    Add missing ocmma

--- db/seeds/role_types.rb
+++ db/seeds/role_types.rb
@@ -6,7 +6,7 @@ ROLE_TYPES = [
   ["stock_plate", "The plate register by manifest that contains one or more samples in this event"],
   ["sample", "A sample which was processed in this event"],
   ["library_source_labware","The plate or tube that entered the library prep process"],
-  ["sequencing_source_labware","The library tube or strip tube entering a sequencing process"]
+  ["sequencing_source_labware","The library tube or strip tube entering a sequencing process"],
   ["labware", "The piece of labware, such as a plate or tube, which was processed in this event"]
 ]
 
